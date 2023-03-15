90a524233cc0307427f40743be1c0bbde6fafe6b
Ye.S <small_black_luo@163.com>
2023-03-15 11:11:26 +0800

    2023-03-15-01-预测版块设计完成

--- TidalPrediction/introduce/templatetags/custom_tag.py
+++ TidalPrediction/introduce/templatetags/custom_tag.py
@@ -15,3 +15,14 @@ def custom_markdown(content):
                          content,
                          1)
     return content
+
+
+@register.filter(name='get_v')
+def get_value(dictionary: dict, key):
+    """
+    根据字典的键获取对应的值
+    :param dictionary: 字典对象
+    :param key: 键
+    :return:
+    """
+    return dictionary.get(key)
